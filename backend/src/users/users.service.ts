49ecdb0a6860d3881a49f28e1f04589ea4d1a994
Abdelrhman Bishr <abdelrhmanwaelbishr017@gmail.com>
2026-09-11 21:56:26 +0300

    feat: implement user listing

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private readonly prisma: PrismaService) { }
+
+    async findAll() {
+        return this.prisma.user.findMany({ select: { id: true, name: true, email: true, role: true, createdAt: true }, orderBy: { createdAt: 'desc' } })
+    }
+}
